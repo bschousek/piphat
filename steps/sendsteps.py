84d6ece4aa1fc0c36e4db4b08e7b02113218f3f2
zpbruno <brian@schousek.com>
2017-05-06 00:39:08 -0500

    Signed-off-by: zpbruno <brian@schousek.com>

--- steps/sendsteps.py
+++ steps/sendsteps.py
@@ -1,24 +1,27 @@
+import logging
+logging.basicConfig(filename='csteps.log', filemode='a', level=logging.DEBUG)
 import numpy as np
 import pigpio
 import time
 pi=pigpio.pi('moby.local')
-import numpy_test as nt
+import numpy_test_unispeed as nt
 
-periodns=10e6
-current_time=np.linspace(0, periodns, num=1000, endpoint=True)
+periodns=1e6
+#current_time=np.linspace(0, periodns, num=1000, endpoint=True)
 #speed=speed=np.ones_like(current_time)/100000 #100 steps per millisecond
-speedparms=[10/1e4,10/1e4]
-
-nsteps=nt.calcsteps(current_time, speedparms)
-wave=nt.build_wave(nsteps['stepdelta'])
-#print(nsteps)
-print len(wave)
-print(nsteps['deltap'])
+#speedparms=[10/1e4,10/1e4]
+speed=-500000
+#nsteps=nt.calcsteps(current_time, speedparms)
+#print(nsteps['deltap'])
 
 axisen=22
 
 pinstep=12
 pindir=6
+nsteps=nt.calcsteps(periodns, speed)
+wave=nt.build_wave(nsteps, dirpin=pindir, steppin=pinstep)
+#print(nsteps)
+print len(wave)
 #pinstep=6
 #pindir=12
 
@@ -58,21 +61,25 @@ def astep():
 # pi.write(pinstep,0)
 # pi.write(pinstep,1)
 pi.write(axisen,0)
+pi.write(pindir,1)
 if False:
-	for n in range(150):
+	for n in range(5):
 		astep()
 pi.wave_clear()
 pi.wave_add_generic(wave)
 wid=pi.wave_create()
-
+logging.debug('wid %i length %i' %(wid,pi.wave_get_pulses()))
+logging.debug('wave time %f' %pi.wave_get_micros())
 print(wid)
 print("constants are")
 print(pigpio.WAVE_MODE_ONE_SHOT_SYNC)
-print(dir(pigpio))
-for x in range(10):
-	pi.wave_send_once(wid)
+#print(dir(pigpio))
+if True:
+	for x in range(22):
+		pi.wave_send_once(wid)
 
-	#pi.wave_send_once(wid)
+else:
+	pi.wave_send_repeat(wid)
 	#pi.wave_send_once(wid)
 #pi.wave_send_using_mode(wid, pigpio.WAVE_MODE_ONE_SHOT_SYNC)
 while pi.wave_tx_busy():
