cd5c028a92db15986ac9116508e97fb88aae3aca
Vagrant Default User <vagrant@jessie.raw>
2017-05-07 06:02:22 +0000

    working change detection one axis

--- machinekit/basic/passthrough.py
+++ machinekit/basic/passthrough.py
@@ -5,13 +5,19 @@ import logging
 logging.basicConfig(filename='passthrough.log', level=logging.DEBUG)
 #logger=logging.getLogger()
 h = hal.component("passthrough")
-h.newpin("in", hal.HAL_FLOAT, hal.HAL_IN)
-h.newpin("out", hal.HAL_FLOAT, hal.HAL_OUT)
+h.newpin("xvel-cmd", hal.HAL_FLOAT, hal.HAL_IN)
+h.newpin("xpos-cmd", hal.HAL_FLOAT, hal.HAL_IN)
+h.newpin("xpos-fb", hal.HAL_FLOAT, hal.HAL_OUT)
 h.ready()
+xwas = 0
 try:
     while 1:
         time.sleep(.01)
-        h['out'] = h['in']
+        if xwas != h['xpos-cmd']:
+        	print("xcmd %f xfb %f xvel %f" %(h['xpos-cmd'], h['xpos-fb'], h['xvel-cmd']))
+        	xwas=h['xpos-cmd']
+        	h['xpos-fb'] = h['xpos-cmd']
+
         #logging.info('out %r in %r' %(h['out'], h['in']))
 except KeyboardInterrupt:
     raise SystemExit
